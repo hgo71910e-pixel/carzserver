0abb94e7b279170a3c2c68c1af6e54a34ae9d7da
hgo71910e-pixel <hgo71910e@gmail.com>
2026-06-27 11:43:17 +0300

    Add files via upload

--- nft-collection.js
+++ nft-collection.js
@@ -4,19 +4,15 @@ async function getSDK() {
   const isTestnet = (process.env.TON_NETWORK || 'testnet') === 'testnet';
   const api = await createApi(isTestnet ? 'testnet' : 'mainnet');
   const keyPair = await importKey(process.env.TON_MINTER_MNEMONIC || '');
-
-  let sender;
-  for (const t of ['v4r2', 'v4', 'highload-v2']) {
-    try { sender = await createSender(t, keyPair, api); break; } catch(e) {}
-  }
-  if (!sender) throw new Error('No working wallet type');
+  const sender = await createSender('highload-v2', keyPair, api);
 
   const storage = {
     pinataApiKey: process.env.PINATA_API_KEY || '',
     pinataSecretKey: process.env.PINATA_SECRET || ''
   };
 
-  const sdk = await AssetsSDK.create({ api, storage, sender });
+  const sdk = AssetsSDK.create({ api, storage, sender });
+  console.log('Minter wallet address:', sdk.sender?.address?.toString());
   return { sdk };
 }
 
@@ -24,11 +20,11 @@ async function deployCollection(name, description) {
   const { sdk } = await getSDK();
   console.log('Deploying NFT collection...');
 
-  // Correct params per source code: name, description, image are top-level
-  const collection = await sdk.createNftCollection(
-    { name, description },
-    { amount: BigInt('100000000') }
-  );
+  const collection = await sdk.deployNftCollection({
+    name,
+    description,
+    image: 'https://ipfs.io/ipfs/bafkreihaclz47kegqv5dbzx3uef6and3bkbx5g7ioefv4uqptxpam'
+  });
 
   const address = collection.address.toString();
   console.log('COLLECTION ADDRESS:', address);
